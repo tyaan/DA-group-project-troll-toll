bf0a90bb9e5af71d7813221c39a4ff2354f7ddaa
Tyaan Singh <singhtyaan@gmail.com>
2024-11-20 12:51:02 +1300

    fixes layout.tsx

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,27 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import Navigation from './Navigation'
 import Footer from './Footer'
-import { useAuth0 } from '@auth0/auth0-react'
-export default function Layout() {
-  const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0()
 
+export default function Layout() {
   return (
     <>
-      <header>
-        <h1>Large Thumb Digital</h1>
-      </header>
-      {isAuthenticated ? (
-          <>
-            <p>Welcome, {user?.given_name}</p>
-            <button onClick={() => logout({ returnTo: window.location.origin })}>Log out</button>
-          </>
-        ) : (
-          <button onClick={() => loginWithRedirect()}>Log in</button>
-        )}
-      <main>
-        <Navigation/>
-        <Outlet />
-      </main>
+      <Navigation/>
+      <Outlet/>
       <Footer/>
     </>
   )
