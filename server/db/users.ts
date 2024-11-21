4d6f2dc0c1ca30d5bcd4ed2fe53f3a84537c8e5f
Katuta Mwila <katutam.mwilam@gmail.com>
2024-11-22 11:00:07 +1300

    save commit

--- server/db/users.ts
+++ server/db/users.ts
@@ -1,6 +1,5 @@
 import db from './connection'; 
-import { User } from '../../models/user';
-import { UserData } from '../../models/users';
+import { User, UserData } from '../../models/user';
 
 export function getUserByAuth0Sub(auth0_sub: string): Promise<User | undefined> {
   return db('users').where({ auth0_sub }).first();
@@ -16,16 +15,16 @@ export async function getUserIdByAuth0Sub(auth0Sub: string): Promise<number | nu
 }
 
 export async function updateUser(userData: UserData): Promise<User> {
-  return await db('users').where({"auth0_sub": userData.auth0_sub}).first()
+  return await db('users').where({"auth0_sub": userData.auth0Sub}).first()
     .update({
-      "auth0_sub": userData.auth0_sub, 
+      "auth0_sub": userData.auth0Sub, 
       "name": userData.name, 
-      "last_name": userData.last_name, 
+      "last_name": userData.lastName, 
       "email": userData.email, 
       "picture": userData.picture, 
-      "active_bridge": userData.active_bridge, 
-      "fav_bridges": userData.fav_bridges, 
-      "total_toll": userData.total_toll
+      "active_bridge": userData.activeBridge, 
+      "fav_bridges": userData.favBridges, 
+      "total_toll": userData.totalToll
     }, ['*'])
 }
 
