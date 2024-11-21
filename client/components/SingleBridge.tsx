f7802c6dba03d23a2537455ed5b66c1eada54793
James McIlraith <jamesmcilraith@gmail.com>
2024-11-22 10:29:53 +1300

    CSS changes

--- client/components/SingleBridge.tsx
+++ client/components/SingleBridge.tsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { getBridge } from '../apis/bridge'
 import Header from './Header'
 import MainContent from './MainContent'
